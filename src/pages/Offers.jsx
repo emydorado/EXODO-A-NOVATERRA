80f292438a6f4296296f1e6935923caf36a436ee
emydorado <145810874+emydorado@users.noreply.github.com>
2025-08-28 10:08:25 -0500

    css offers

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,13 +1,27 @@
+import SavedHumans from '../components/SavedHumans';
+import Rounds from '../components/Rounds';
 import OfferCard from '../components/OfferCard';
 import offers from '../data/offers';
+import './offers.css';
 
 const Offers = () => {
 	return (
-		<>
+		<div id='offers'>
+			<div className='counters'>
+				<SavedHumans />
+				<Rounds />
+			</div>
+			<h2>OFFERS</h2>
 			{offers.map((offer) => (
 				<OfferCard key={offer.id} capacity={offer.capacity} cost={offer.cost} />
 			))}
-		</>
+			<div className='timer'>
+				<h3>
+					<b>Tiempo Restante</b>
+				</h3>
+				<p>20 segundos</p>
+			</div>
+		</div>
 	);
 };
 
